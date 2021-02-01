3158531ad0c67893696ebb9b2a70a8ae6973872f
Amin-MAG <M.Amin.Ghasvari@Gmail.com>
2021-02-01 21:14:03 +0330

    Add global descriptor table

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,6 @@
 #include "types.h"
+#include "gdt.h"
+
 
 void printf(char* str) {
     uint16_t* VideoMemory = (uint16_t*)0xb8000;
@@ -19,5 +21,9 @@ extern "C" void callConstructors() {
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t magicnumber) {
     printf("Hello World !!");
+
+    GlobalDescriptorTable gdt; 
+
+
     while (1);
 }
